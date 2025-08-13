24f7799ce828acff4605c56a649f45575cdbf4c0
chowh0823 <ccc2900@gmail.com>
2025-08-13 17:55:08 +0900

    algo stack1_2

--- Stack1_2/4869_stick_paper.py
+++ Stack1_2/4869_stick_paper.py
@@ -1,3 +1,4 @@
+
 import sys
 
 sys.stdin = open('input.txt', 'r')
@@ -5,4 +6,12 @@ sys.stdin = open('input.txt', 'r')
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
+    num = N//10
+    result = [0 for _ in range(num+1)]
 
+    result[1] = 1
+    result[2] = 3
+    for i in range(3, num+1):
+        result[i] = result[i-1] + 2*result[i-2]
+
+    print(f"#{tc} {result[num]}")
